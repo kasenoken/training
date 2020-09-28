af9f7b3fc68b84bd89d0fb15f36c02d97d229cac
Victor Vinicius <44174141+kasenoken@users.noreply.github.com>
2020-09-28 15:12:36 -0300

    Update First.java

--- First.java
+++ First.java
@@ -13,21 +13,3 @@ public class Main {
     static void train(int x, int y) {
         System.out.println(x + y);
     }
-
-}package com.company;
-
-public class Main {
-
-    public static void main(String[] args) {
-        train(17, 22);
-        train(4,8);
-        train(7, 17);
-        train(8, 32);
-        train(44, 9);
-    }
-
-    static void train(int x, int y) {
-        System.out.println(x + y);
-    }
-
-}
