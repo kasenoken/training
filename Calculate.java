ae1c3a54707c7e2d1092f19761d567d61308c196
Victor Vinicius <44174141+kasenoken@users.noreply.github.com>
2020-10-07 20:00:08 -0300

    Create Calculate.java

---
+++ Calculate.java
@@ -0,0 +1,21 @@
+public class Calculate {
+
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[1]);
+        int y = Integer.parseInt(args[2]);
+
+        if (args[0].equals("Somar")) {
+            sum(x, y);
+        } else if (args[0].equals("Subtrair")) {
+            minus(x, y);
+        } else {
+            System.out.println("Nenhuma instrução definida");
+        }
+    }
+    static void sum(int x, int y){
+        System.out.println(x + y);
+    }
+    static void minus(int x, int y) {
+        System.out.println(x - y);
+    }
+}
